60300d6171ff8567b7b826bd3b4f84afb302596f
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-18 16:33:11 +0300

    feat(keys naming): automated key gereation

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,16 +1,17 @@
 import { client } from '$services/redis';
+import { pageCacheKey } from '$services/keys';
 
 const caacheRoutes = ['/about', '/privacy', '/auth/signin', '/auth/signup'];
 export const getCachedPage = (route: string) => {
 	if (caacheRoutes.includes(route)) {
-		return client.get('pagecache#' + route);
+		return client.get(pageCacheKey(route));
 	}
 	return null;
 };
 
 export const setCachedPage = (route: string, page: string) => {
 	if (caacheRoutes.includes(route)) {
-		return client.set('pagecache#' + route, page, {
+		return client.set(pageCacheKey(route), page, {
 			EX: 2
 		});
 	}
