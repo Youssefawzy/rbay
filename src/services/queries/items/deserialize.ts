709dbf77f8c825ec9a40f9138dbcf9a9588fa490
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-19 16:03:51 +0300

    feat(items hash): get items

--- src/services/queries/items/deserialize.ts
+++ src/services/queries/items/deserialize.ts
@@ -1,4 +1,21 @@
 import type { Item } from '$services/types';
 import { DateTime } from 'luxon';
 
-export const deserialize = (id: string, item: { [key: string]: string }): Item => {};
+export const deserialize = (id: string, item: { [key: string]: string }): Item => {
+	return {
+		id,
+		name: item.name,
+		description: item.description,
+		imageUrl: item.imageUrl,
+		highestBidUserId: item.highestBidUserId,
+		ownerId: item.ownerId,
+    //they are stored as millis string 
+		createdAt: DateTime.fromMillis(parseInt(item.createdAt)),
+		endingAt: DateTime.fromMillis(parseInt(item.endingAt)),
+    //they are stored as string
+		views: parseInt(item.views),
+		likes: parseInt(item.likes),
+		bids: parseInt(item.bids),
+		price: parseFloat(item.price)
+	};
+};
