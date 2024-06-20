4e1880fe76ebda68df98f8ddd537705ec2a88e1a
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-20 16:19:13 +0300

    feat(users): add signup users to a sorted set

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,7 +1,7 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
 import { client } from '$services/redis';
-import { usersKey, usernamesUniqueKey } from '$services/keys';
+import { usersKey, usernamesUniqueKey, usernamesKey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
@@ -12,7 +12,7 @@ export const getUserById = async (id: string) => {
 };
 
 export const createUser = async (attrs: CreateUserAttrs) => {
-	const id = genId();
+	const id = genId();//return hex decimal as string
 
 	const exists = await client.sIsMember(usernamesUniqueKey(), attrs.username);
 
@@ -22,6 +22,10 @@ export const createUser = async (attrs: CreateUserAttrs) => {
 
 	await client.hSet(usersKey(id), serialize(attrs));
 	await client.sAdd(usernamesUniqueKey(), attrs.username);
+	await client.zAdd(usernamesKey(), {
+		value: attrs.username,
+		score: parseInt(id, 16)
+	});
 
 	return id;
 };
