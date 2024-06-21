21663670743673a0f8b8cf0d2bad231fc5189134
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-21 23:23:15 +0300

    feat(bids): create new bid

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,7 +1,26 @@
+import { client } from '$services/redis';
 import type { CreateBidAttrs, Bid } from '$services/types';
+import { DateTime } from 'luxon';
+import { bidHistoryKey } from '$services/keys';
 
-export const createBid = async (attrs: CreateBidAttrs) => {};
+export const createBid = async (attrs: CreateBidAttrs) => {
+	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+
+	return client.rPush(bidHistoryKey(attrs.itemId), serialized);
+};
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
 	return [];
+};
+
+const serializeHistory = (amount: number, createdAt: number) => {
+	return `${amount}:${createdAt}`;
+};
+
+const deserializeHistory = (stored: string) => {
+	const [amount, createdAt] = stored.split(':');
+	return {
+		amount: parseFloat(amount),
+		createdAt: DateTime.fromMillis(parseInt(createdAt))
+	};
 };
