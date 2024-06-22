fa944975f9e2325586c4e89fb3faf2cf18603b4b
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-22 15:46:28 +0300

    fix(bids): solving concurrency issue

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,25 +1,37 @@
 import { client } from '$services/redis';
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { DateTime } from 'luxon';
-import { bidHistoryKey } from '$services/keys';
+import { bidHistoryKey, itemsKey } from '$services/keys';
 import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	const item = await getItem(attrs.itemId);
+	return client.executeIsolated(async (isolatedClient) => {
+		await isolatedClient.watch(itemsKey(attrs.itemId));
 
-	if (!item) {
-		throw new Error('Item does not exist');
-	}
-	if (item.price >= attrs.amount) {
-		throw new Error('Bid too low');
-	}
-	if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
-		throw new Error('item close to bidding');
-	}
+		const item = await getItem(attrs.itemId);
 
-	const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+		if (!item) {
+			throw new Error('Item does not exist');
+		}
+		if (item.price >= attrs.amount) {
+			throw new Error('Bid too low');
+		}
+		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
+			throw new Error('item close to bidding');
+		}
 
-	return client.rPush(bidHistoryKey(attrs.itemId), serialized);
+		const serialized = serializeHistory(attrs.amount, attrs.createdAt.toMillis());
+
+		return isolatedClient
+			.multi()
+			.rPush(bidHistoryKey(attrs.itemId), serialized)
+			.hSet(itemsKey(item.id), {
+				bids: item.bids + 1,
+				price: attrs.amount,
+				highestBidUserId: attrs.userId
+			})
+			.exec();
+	});
 };
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
